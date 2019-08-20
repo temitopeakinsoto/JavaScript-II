b3219ffaee5227e646d3d5c3ca761c0585905a9e
Temitope Akinsoto <takinsoto@gmail.com>
2019-08-20 15:02:25 +0100

    feat: Create higher order functions
    in this commit, i created the following higher order functions getLength(), last(), sumNums(), multiplyNums() and contains()
    1. getLength passes the length of the array into the callback.
    2.last passes the last item of the array into the callback.last passes the last item of the array into the callback.
    3.sumNums adds two numbers (x, y) and passes the result to the callback.
    4.multiplyNums multiplies two numbers and passes the result to the callback.
    5.contains checks if an item is present inside of the given array/list.  Pass true to the callback if it is, otherwise pass false.
    
    TASKS IN callback.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,23 +41,36 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  let indexOfLastItem = arr.length - 1;
+  return cb(arr[indexOfLastItem]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let resultOfSum = x + y;
+  return cb(resultOfSum);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let resultOfMultiplication = x * y;
+  return cb(resultOfMultiplication);
 }
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
+  // contains checks if an item is present inside of the given array/list.  
   // Pass true to the callback if it is, otherwise pass false.
+  if(list.includes(item) === true){
+    return cb(true);
+  }else{
+    return cb(false);
+  }
+
 }
 
 /* STRETCH PROBLEM */
